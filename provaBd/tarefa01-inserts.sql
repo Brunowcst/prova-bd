d9e92d9fd6d3ac31d5034de4c88453381bc322fd
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-04-27 18:40:46 -0300

    Insert tables #1

--- provaBd/tarefa01-inserts.sql
+++ provaBd/tarefa01-inserts.sql
@@ -1,80 +1,51 @@
+--Inserir em departamentos
+INSERT INTO departamento (descricao, cod_gerente) VALUES
+('Vendas', null),
+('Marketing', null),
+('TI', null),
+('RH', null),
+('Contabilidade', null);
+
+--INSERINDO GERENTES
+INSERT INTO funcionario
+(nome, sexo, dt_nasc, salario, cod_depto)
+VALUES ('Bruno', 'F', '1988-05-07', 2500.00, 1),
+('David', 'M', '1980-01-25', 2500.00, 2),
+('Renan', 'M', '1970-07-18', 2500.00, 3);
+
+UPDATE departamento SET cod_gerente = 1 WHERE codigo = 5;
+UPDATE departamento SET cod_gerente = 2 WHERE codigo = 4;
+UPDATE departamento SET cod_gerente = 3 WHERE codigo = 3;
+
 --INSERIR em funcionario
-
-INSERT INTO funcionario(nome, sexo, dt_nasc, salario, cod_depto)
-VALUES ('João', 'M', '1990-02-14', 5000.00, 1);
-
-INSERT INTO funcionario( nome, sexo, dt_nasc, salario, cod_depto)
-VALUES ('Maria', 'F', '1988-09-23', 6500.00, 2);
-
-INSERT INTO funcionario( nome, sexo, dt_nasc, salario, cod_depto)
-VALUES ('Pedro', 'M', '1995-06-10', 4500.00, 1);
-
-INSERT INTO funcionario( nome, sexo, dt_nasc, salario, cod_depto)
-VALUES ('Ana', 'F', '1992-11-27', 5500.00, 3);
-
-INSERT INTO funcionario( nome, sexo, dt_nasc, salario, cod_depto)
-VALUES ('Lucas', 'M', '1991-04-05', 7000.00, 2);
-
-
---Inserir departamentos
-INSERT INTO departamento( descricao, cod_gerente)
-VALUES ('Vendas', 2);
-
-INSERT INTO departamento( descricao, cod_gerente)
-VALUES ('Desenvolvimento', 5);
-
-INSERT INTO departamento( descricao, cod_gerente)
-VALUES ('Financeiro', 4);
-
-INSERT INTO departamento( descricao, cod_gerente)
-VALUES ('Marketing', 3);
-
-INSERT INTO departamento( descricao, cod_gerente)
-VALUES ('RH', 1);
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario, cod_depto) VALUES
+('João', 'M', '1990-01-01', 3000.00, 1),
+('Maria', 'F', '1995-05-10', 3500.00, 2),
+('Pedro', 'M', '1985-12-25', 4000.00, 1),
+('Ana', 'F', '1999-08-15', 2800.00, 3),
+('Lucas', 'M', '1992-06-30', 3200.00, 2);
 
 --INSERIR em projeto
-
-INSERT INTO projeto(codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
-VALUES (1, 'Projeto A', 'Desenvolvimento de um novo software', 2, 5, '2022-01-01', '2022-06-30');
-
-INSERT INTO projeto(codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
-VALUES (2, 'Projeto B', 'Implementação de uma nova estratégia de marketing', 4, 3, '2022-03-15', '2022-11-30');
-
-INSERT INTO projeto(codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
-VALUES (3, 'Projeto C', 'Análise de mercado e estudo de concorrência', 4, 2, '2022-05-01', '2022-09-30');
-
-INSERT INTO projeto(codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
-VALUES (4, 'Projeto D', 'Implantação de novo sistema de gerenciamento financeiro', 3, 4, '2022-06-01', '2022-12-31');
-
-INSERT INTO projeto(codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
-VALUES (5, 'Projeto E', 'Desenvolvimento de campanha publicitária', 4, 3, '2022-02-01', '2022-08-31');
+INSERT INTO projeto (nome, descricao, cod_responsavel, cod_depto, data_inicio, data_fim) VALUES
+('Projeto A', 'Descrição do Projeto A', 1, 3, '2022-01-01', '2022-12-31'),
+('Projeto B', 'Descrição do Projeto B', 2, 2, '2022-03-01', '2022-08-31'),
+('Projeto C', 'Descrição do Projeto C', 3, 1, '2022-06-01', '2022-11-30'),
+('Projeto D', 'Descrição do Projeto D', 1, 1, '2022-02-01', '2022-10-31'),
+('Projeto E', 'Descrição do Projeto E', 4, 4, '2022-04-01', '2022-07-31');
 
 
 ----INSERIR em atividade
+INSERT INTO atividade (nome, descricao, cod_responsavel, data_inicio, data_fim) VALUES
+('Atividade A1', 'Descrição da Atividade A1', 1, '2022-01-01', '2022-03-31'),
+('Atividade A2', 'Descrição da Atividade A2', 2, '2022-02-01', '2022-04-30'),
+('Atividade B1', 'Descrição da Atividade B1', 3, '2022-03-01', '2022-06-30'),
+('Atividade C1', 'Descrição da Atividade C1', 1, '2022-04-01', '2022-08-31'),
+('Atividade D1', 'Descrição da Atividade D1', 4, '2022-05-01', '2022-07-31');
 
-INSERT INTO atividade (nome, descricao, cod_responsavel, data_inicio, data_fim)
-VALUES ('Atividade 1', 'criando atividade 1 ', 1, '2023-05-01', '2023-05-15');
-
-INSERT INTO atividade (nome, descricao, cod_responsavel, data_inicio, data_fim)
-VALUES ('Atividade 2', 'Criando atividade 2', 2, '2023-06-01', '2023-06-15');
-
-INSERT INTO atividade (nome, descricao, cod_responsavel, data_inicio, data_fim)
-VALUES ('Atividade 3', 'Criando atividade 3', 3, '2023-07-01', '2023-07-15');
-
-INSERT INTO atividade (nome, descricao, cod_responsavel, data_inicio, data_fim)
-VALUES ('Atividade 4', 'Criando atividade 4', 4, '2023-08-01', '2023-08-15');
-
-INSERT INTO atividade (nome, descricao, cod_responsavel, data_inicio, data_fim)
-VALUES ('Atividade 5', 'Criando atividade 5', 5, '2023-08-01', '2023-08-15');
-
---Inserir atividades_projetos
-INSERT INTO atividade_projeto (cod_projeto, cod_atividade)
-VALUES (1, 1);
-INSERT INTO atividade_projeto (cod_projeto, cod_atividade)
-VALUES (2, 2);
-INSERT INTO atividade_projeto (cod_projeto, cod_atividade)
-VALUES (3, 3);
-INSERT INTO atividade_projeto (cod_projeto, cod_atividade)
-VALUES (4, 4);
-INSERT INTO atividade_projeto (cod_projeto, cod_atividade)
-VALUES (5, 5);
+--Inserir em atividades_projetos
+INSERT INTO atividade_projeto (cod_projeto, cod_atividade) VALUES
+(1,1),
+(1,2),
+(2,3),
+(3,4),
+(4,1);
