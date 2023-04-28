2d68b7f9dbef95558f9fe66eb143545d8838a63d
Brunowcst <bruno.costa.079@ufrn.edu.br>
2023-04-27 23:15:22 -0300

    Questão 4 att. #1

--- provaBd/Consultas/tarefa01-q04.sql
+++ provaBd/Consultas/tarefa01-q04.sql
@@ -1,10 +1,7 @@
-CREATE VIEW funcNaoGerente AS
-SELECT f.nome, f.salario, d.descricao AS departamento
+--QUESTÃO 04: Faça uma consulta que selecione o nome, o salário e o departamento dos funcionários que não são gerentes, ordenando pelo Código do Departamento.
+
+SELECT f.nome, f.salario, d.descricao
 FROM funcionario f
 JOIN departamento d ON f.cod_depto = d.codigo
 WHERE f.codigo NOT IN (SELECT cod_gerente FROM departamento WHERE cod_gerente IS NOT NULL)
-ORDER BY f.cod_depto;
-
-
---Consulta
-SELECT * FROM funcNaoGerente;
+ORDER BY d.codigo;
